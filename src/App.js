de3c2b2899dcbcf1cc7cd4ff10a69a0c3e61a536
Daniel Butler <dabutler89@gmail.com>
2019-07-05 15:37:45 -0400

    template of clock and timer created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CountDownClock extends React.Component {
+    render() {
+        return (
+            <div className="count-down-clock-wrapper">
+              <div className="count-down-clock">
+                {/* TODO */}
+              </div>
+            </div>
+        )
+    };
 }
+
+class CountDownInput extends React.Component {
+    render() {
+        return (
+            <div className="count-down-input-wrapper">
+              <p>Enter Time in Minutes</p>
+              <input
+                  type="number"
+                  className="count-down-input"
+                  min="1"
+              />
+              <p>Warning will displayed after {/* TODO: Fill in Time */} minute(s).</p>
+            </div>
+        )
+    };
+}
+
+class CountDownControl extends React.Component {
+    render() {
+        return (
+            <div className="count-down-control">
+                <button
+                    className="count-down-start"
+                >
+                  <i className="material-icons">play_arrow</i>
+                </button>
+                <button
+                    className="count-down-pause"
+                >
+                  <i className="material-icons">pause</i>
+                </button>
+                <button
+                    className="count-down-stop"
+                >
+                  <i className="material-icons">stop</i>
+                </button>
+                <button
+                    className="count-down-restart"
+                >
+                  <i className="material-icons">repeat</i>
+                </button>
+            </div>
+        )
+    };
+}
+
+
+class CountDownDisplay extends React.Component {
+    render() {
+        return (
+            <div className="count-down-display count-display-green"></div>
+        )
+    };
+}
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className="app">
+          <CountDownDisplay/>
+          <CountDownClock/>
+          <CountDownInput/>
+          <CountDownControl/>
+        </div>
+    )
+  }
+}
+
 
 export default App;
